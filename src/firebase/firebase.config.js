21f24afe9a17a171521b9344bfe05e491bc44551
Mehedi Hasan <mehedi@99robots.com>
2025-11-13 20:52:53 +0600

    env changes

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHce-FJXZdEabxO4TgiA326IpPRVCqnBk",
-  authDomain: "freelance-marketplace-ce615.firebaseapp.com",
-  projectId: "freelance-marketplace-ce615",
-  storageBucket: "freelance-marketplace-ce615.firebasestorage.app",
-  messagingSenderId: "702970971318",
-  appId: "1:702970971318:web:f292b1e2d883833c69d217",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
